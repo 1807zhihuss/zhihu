89bcfdd99e867239b93d2fe4f454dd9aab643473
xixin <2793030875@qq.com>
2019-08-08 20:31:02 +0800

    'login'

--- zhihu/src/components/app/index.jsx
+++ zhihu/src/components/app/index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import Login from '../login'
 export default class app extends Component {
     render() {
         return (
             <div>
-                我是App
+                {/* 登录 */}
+                <Login />
             </div>
         )
     }
