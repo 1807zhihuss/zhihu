77a8f3b8920e77802230f6aeb76ea0f403167e42
chenxueliu <786183405@qq.com>
2019-08-09 16:07:21 +0800

    'cxl'

--- zhihu/src/components/app/index.jsx
+++ zhihu/src/components/app/index.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-
+import Common from '../content/common'
+import Weibo from '../content/weibo'
 export default class app extends Component {
     render() {
         return (
             <div>
-                我是App
+                {/*二级公共模板 */}
+                {/* <Common /> */}
+                {/* 微博热搜 */}
+                <Weibo />
             </div>
         )
     }
