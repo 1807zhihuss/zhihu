426bec41819a28c45d534857bac0997b355ae98f
chenxueliu <786183405@qq.com>
2019-08-12 09:59:30 +0800

    'cxl'

--- zhihu/src/components/content/weibo/index.jsx
+++ zhihu/src/components/content/weibo/index.jsx
@@ -6,6 +6,18 @@ export default class Weibo extends Component {
     render() {
         return (
             <div className='cxl-wb'>
+                <header className="header-box ">
+                    <div className='header-top flex flex-ju'>
+                        <div className="hed-top">知乎</div>
+                        <div className="sou-put">
+                            🔍
+                            <input type="text" className='inp' placeholder='搜索知乎内容' />
+                        </div>
+                        <div className="size-box">
+                            取消
+                    </div>
+                    </div>
+                </header>
                 <img src={img} alt="" />
             </div>
         )
